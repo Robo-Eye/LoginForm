a52c02644ded38a28c7244863a1476b535291c1c
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-06-29 20:42:33 -0400

    Commit With Java

---
+++ ConsoleLogin/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        try{
+            System.out.println("Please enter a valid username:");
+        }catch(Exception e){
+
+        }
+
+
+    }
+}
