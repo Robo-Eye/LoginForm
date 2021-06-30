1ef6db9db38dc7bbcdf0df8843640cc0b6542d79
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-06-30 11:40:49 -0400

    Added Test Code to Main... Needs work...

--- ConsoleLogin/src/Main.java
+++ ConsoleLogin/src/Main.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an Username:");
+        Username u = new Username();
+        String user = sc.next();
+        u.setUsername(user);
+        System.out.println("Your Username Is: " + u.getUsername());
     }
 }
--- ConsoleLogin/src/Username.java
+++ ConsoleLogin/src/Username.java
@@ -13,7 +13,7 @@ public class Username {
     public void setUsername(String username) throws Exception {
         if(username.contains("'") || username.contains("\"") || username.contains(" ")){
             throw new Exception("Username cannot contain ', \", or Empty Spaces (use _ instead)");
-        }else if(username.equals("")) {
+        }else if(username.equals(" ")) {
             throw new Exception("Invalid Username");
         }else if(this.username.equals(username)){
             throw new Exception("Username Taken");
