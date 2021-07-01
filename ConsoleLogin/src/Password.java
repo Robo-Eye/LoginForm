9b86a261730460e8f62cc1715a47dd019c77e978
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-07-01 18:30:04 -0400

    Comments

--- ConsoleLogin/src/Password.java
+++ ConsoleLogin/src/Password.java
@@ -17,6 +17,8 @@ public class Password {
     }
 
     public boolean regexChecker(String theStrBeingChecked){
+        //Current Requirements ( At LEAST One number, One Lowercase Char, One Uppercase Char,
+        //One Special Symbol "@#$%", and be between 8 and 40 characters.
         String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,40}$";
         Pattern checkRegex = Pattern.compile(regex);
 
