052565a8ae3db80456188de10d94227ca2f7254b
Robo-Eye <68978948+Robo-Eye@users.noreply.github.com>
2021-07-01 15:44:56 -0400

    Setters, Getters, and Constructors for Password Class

--- ConsoleLogin/src/Password.java
+++ ConsoleLogin/src/Password.java
@@ -2,5 +2,20 @@ import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Password {
+    private String password;
+
+    public Password(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 
 }
